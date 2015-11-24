f6f3707882dbe88749b0f59067692531a9587db7
whocares <liuyibing123456@126.com>
2015-11-24 09:31:13 +0800

    modified:   weather/src/main/java/org/whocares/weather/service/impl/CityServiceImpl.java 	modified:   weather/src/main/resources/applicationContext.xml 	new file:   weather/src/main/webapp/css/global.css 	modified:   weather/src/main/webapp/html/city.html 	modified:   weather/src/main/webapp/html/hourly.html 	modified:   weather/src/main/webapp/html/index.html 	new file:   weather/src/main/webapp/html/main.html

--- weather/src/main/java/org/whocares/weather/service/impl/CityServiceImpl.java
+++ weather/src/main/java/org/whocares/weather/service/impl/CityServiceImpl.java
@@ -1,18 +1,19 @@
 package org.whocares.weather.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.whocares.weather.dao.ICityDao;
 import org.whocares.weather.entity.city.City;
 import org.whocares.weather.service.ICityService;
 import org.whocares.weather.util.GlobalUtils;
 
 @Service("cityService")
+//@Transactional(readOnly = true)
 public class CityServiceImpl implements ICityService {
 
 	@Autowired
@@ -36,18 +37,19 @@ public class CityServiceImpl implements ICityService {
 	}
 
 	@Override
+//	@Transactional(readOnly = false, rollbackFor = Exception.class)
 	public void cacheCityInfo() {
 		String jsonStr = GlobalUtils.readFileAsString(GlobalUtils.CITYCODE_JSON);
-		LOGGER.debug(GlobalUtils.CITYCODE_JSON + "\n" + jsonStr);
-		//@SuppressWarnings("unchecked")
-		ArrayList<City> list = GlobalUtils.parseResponseJson(jsonStr, ArrayList.class);
+		LOGGER.debug("file { " + GlobalUtils.CITYCODE_JSON + " } : " + jsonStr);
+
+		List<City> list = GlobalUtils.parseResponseListJson(jsonStr, City.class);
 		
 		for(City city : list) {
 			cityDao.cacheProvinces(city.getProvinceCN());
 			
 			cityDao.cacheCitys(String.valueOf(city.getProvinceCN().hashCode()), city.getCityCN());
 			
-			City instance = new City.Builder(city.getDistrictId(), city.getDistrictEN(), city.getCityCN()).build();
+			City instance = new City.Builder(city.getDistrictId(), city.getDistrictEN(), city.getDistrictCN()).build();
 			cityDao.cacheDistricts(String.valueOf(city.getProvinceCN().hashCode()), 
 					String.valueOf(city.getCityCN().hashCode()), GlobalUtils.parseOject2Json(instance));
 		}
